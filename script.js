a0ba98b46f4d332b454d36b529d36714abd969fd
Hiamni <mahenderverma646@gmail.com>
2025-07-30 12:02:41 +0530

    formatted

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let task=[];
-document.getElementById("newtask").addEventListener("click",function(e.preventDefault()){
 
-})
+const addtask=()=>{
+    const taskinput=document.getElementById('taskinput');
+    const text=taskinput.value.trim();
+if(text){
+    task.push({text:text,completed:false});
+}
+console.log(task);
+};
+
+
+document.getElementById("newtask").addEventListener("click",function(e){
+    e.preventDefault();
+    addtask();
+}
+
+);
+
+
