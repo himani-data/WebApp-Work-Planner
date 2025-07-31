e2cd017328c7d9cfbb33888f1ab79d2639a04f72
Hiamni <mahenderverma646@gmail.com>
2025-07-31 06:55:50 +0530

    colors

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ let task=[];
 
 const addtask=()=>{
     const taskinput=document.getElementById('taskinput');
-    const text=taskinput.value.trim();
+    const text=taskInput.value.trim();
 if(text){
     task.push({text:text,completed:false});
+    taskInput.value="";
+    updateTaskList();
 }
-console.log(task);
 };
 
+const updateTaskList=()=>{
+    const taskList =document.getElementById('task')
+    task.innerHTML=''
+
+    task.forEach(task=>{
+        const listItem
+    })
+}
 
 document.getElementById("newtask").addEventListener("click",function(e){
     e.preventDefault();
