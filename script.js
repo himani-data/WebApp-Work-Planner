fec80ca60d8f8d333570bf2a705b68b63d5131ea
Hiamni <mahenderverma646@gmail.com>
2025-07-31 16:19:22 +0530

    confetti added

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  const storedTasks =JSON.parse(localStorage.getItem('tasks'))
+
+  if(storedTasks){
+    storedTasks.forEach((tasks)=>tasks.push(tasks))
+  updateTaskList();
+  updateStats();
+  }
+
+})
+
 let tasks = [];
+const saveTask=()=>{
+  localStorage.setItem('tasks',JSON.stringify(tasks))
+}
 
 const addtask = () => {
   const taskinput = document.getElementById("taskinput");
@@ -8,6 +22,7 @@ const addtask = () => {
     taskinput.value = "";
     updateTaskList();
     updateStats();
+    saveTask();
   }
 };
 
@@ -48,14 +63,13 @@ document.getElementById("newtask").addEventListener("click", function (e) {
 
 // Stub functions for editTask, deleteTask, toggleTaskComplete
 function editTask(index) {
-  // TODO: Implement edit task functionality
-//   console.log("Edit task at index:", index);
 const taskinput=document.getElementById('taskinput')
 taskinput.value=tasks[index].text
 
 tasks.splice(index,1);{
 updateTaskList();
   updateStats();
+  saveTask();
 }
 
 }
@@ -64,6 +78,7 @@ function deleteTask(index) {
   tasks.splice(index, 1);
   updateTaskList();
   updateStats();
+  saveTask();
 }
 
 function toggleTaskComplete(index) {
@@ -71,6 +86,7 @@ function toggleTaskComplete(index) {
   tasks[index].completed = !tasks[index].completed;
   updateTaskList();
   updateStats();
+  saveTask();
 }
 
 
@@ -78,13 +94,63 @@ const updateStats = () => {
   const completeTasks = tasks.filter((task) => task.completed).length;
   const totalTask = tasks.length;
   const progressBar = document.getElementById("progress");
+  const progress = totalTask === 0 ? 0 : (completeTasks / totalTask) * 100;
 
  
 
  progressBar.style.width = `${progress}%`;
    
-  
+    // Optional: Update number count
+  document.getElementById("number").innerText = `${completeTasks}/${totalTask}`;
+  if(tasks.length && completeTasks==totalTask){
+    blast();
+  }
+};
   
 
+
+
   
-};
+
+const blast=()=>{
+  const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+
+}
