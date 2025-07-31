98590736e784cede210669893fdf9a5eacf0f61e
Hiamni <mahenderverma646@gmail.com>
2025-07-31 11:26:41 +0530

    new js

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-let task=[];
+let tasks=[];
 
 const addtask=()=>{
     const taskinput=document.getElementById('taskinput');
-    const text=task.value.trim();
+    const text=taskinput.value.trim();
 if(text){
-    task.push({text:text,completed:false});
-    task.value="";
+    tasks.push({text:text,completed:false});
+    tasks.value="";
     updateTaskList();
 }
 };
 
 const updateTaskList=()=>{
-    const taskList =document.getElementById('task')
-    task.innerHTML=''
+    const taskList =document.getElementById('tasks')
+    taskList.innerHTML="";
 
-    task.forEach(task=>{
-        const listItem
-    })
-}
+    tasks.forEach((task,index)=>{`
+        const listItem=document.createElement('li');
+
+        listItem.innerHTML=
+        <div class="taskItem">
+        <div class="task ${task.completed? "completed" : ""}">
+            <input type ="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
+            <p>${task.text}</p>
+        </div>
+
+        <div class ='icons'>
+            <img src="./edit.png" onClick="editTask(${index})" />
+            <img src="./bin.png" onClick="deleteTask(${index})" />
+        </div>
+        </div>
+        `;
+       listItem.addEventListener('change',()=> toggleTaskComplete(index));
+       taskList.append(listItem);
+    });
+};
 
 document.getElementById("newtask").addEventListener("click",function(e){
     e.preventDefault();
